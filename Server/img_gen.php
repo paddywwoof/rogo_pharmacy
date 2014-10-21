2fc4527cef889673ba96b69bc74bece0bab4ea74
paddywwoof <patrick@eldwick.org.uk>
2014-10-21 07:28:33 +0100

    hopefully final version for trials

--- Server/img_gen.php
+++ Server/img_gen.php
@@ -6,13 +6,13 @@ $w = $_GET['w'];
 $h = $_GET['h'];
 $im = @imagecreatefromjpeg('images/'.$imgstr.'.jpg');
 if ($im) {
-  $red = imagecolorallocate($im, 255, 0, 0);
+  $red = imagecolorallocatealpha($im, 255, 0, 0, 51);
   if (!is_array($x)){ //only one rectangle
-    imagerectangle($im, $x, $y, $x + $w, $y + $h, $red);
+    imagefilledrectangle($im, $x, $y, $x + $w, $y + $h, $red);
   }
   else {  
     foreach ($x as $i=>$val) {
-      imagerectangle($im, $x[$i], $y[$i], $x[$i] + $w[$i], $y[$i] + $h[$i], $red);
+      imagefilledrectangle($im, $x[$i], $y[$i], $x[$i] + $w[$i], $y[$i] + $h[$i], $red);
     }
   }
   header('Content-Type: image/jpeg');
